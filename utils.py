1c492b4435d710e01c720a6fd16a78403ea93625
James Watson <james.watson-2@colorado.edu>
2024-10-27 23:08:09 -0600

    MANY vector store added

--- utils.py
+++ utils.py
@@ -1,15 +1,64 @@
 ########## INIT ####################################################################################
 
 ### Standard ###
-import json, os, base64, io
+import json, os, base64, io, shutil
+from os import environ, path, makedirs, walk
 from uuid import uuid4
 
 ### Special ###
 import fitz, pymupdf
 from PIL import Image
 
+########## DATABASE OPS ############################################################################
 
 
+def copy_pdfs( source_dir, dest_dir, N_add, verbose = 1  ):
+    """ Recursively copy PDFs according to the env vars """
+    # https://chatgpt.com/share/67156f25-6a98-800d-9c9b-a4b8184cdd46
+    # source_dir = environ["_RAG_PDF_SOURCE"]
+    # dest_dir   = environ["_RAG_PDF_DESTIN"]
+    # verbose    = bool( environ["_RAG_VERBOSE"] )
+    # count      = int( environ["_RAG_DOCDB_COUNT" ] )
+    # rem        = int( environ["_RAG_DOCDB_REMAIN"] )
+
+    # Create destination directory if it doesn't exist
+    if not path.exists( dest_dir ):
+        makedirs( dest_dir )
+
+    # Walk through the source directory
+    i =  0
+    d = 50
+    for dirpath, dirnames, filenames in walk( source_dir ):
+        dirnames.sort()  # Sort subdirectories in-place
+        filenames.sort()  # Sort files in-place
+        for file in filenames:
+            if file.lower().endswith('.pdf') and (N_add > 0):
+                i += 1
+                if (not verbose) and (i % d == 0):
+                    print( '.', end='', flush = True )
+                
+                # Full path of the source PDF file
+                src_file = path.join( dirpath, file )
+                
+                # Copy the PDF to the destination directory
+                try:
+                    destPath = path.join( dest_dir, file )
+                    if not path.isfile( destPath ):
+                        shutil.copy( src_file, dest_dir )
+                        if verbose:
+                            print(f"Copied: {src_file}")
+                        N_add -= 1
+                    else:
+                        if verbose:
+                            print(f"Exists: {destPath}")
+                except Exception as e:
+                    print(f"Error copying {src_file}: {e}")
+                
+    if not verbose:
+        print()
+
+
+    
 ########## APPLICATION STATE #######################################################################
 
 
@@ -24,7 +73,7 @@ class RAG_State:
                 dct = json.load(f)
             if dct is not None:
                 return RAG_State(
-                    docPaths   = dct['libDocs']
+                    docPaths   = dct['libDocs'],
                     pageImages = dct['pages']
                 )
         except FileNotFoundError as e:
